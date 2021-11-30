a5bd81ed80871fc62f6af51ed1d2e369c9fbc2cb
manpin <cz3325@gmail.com>
2021-12-01 02:03:54 +0900

    update

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>WilliamGladstone株式会社</title>
+    <meta charset="UTF-8">
+    <meta name="keywords" keywords="WilliamGladstone株式会社">
+    <meta name="description" content="WilliamGladstone株式会社">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:site_name" content="WilliamGladstone株式会社">
+    <meta property="og:title" content="WilliamGladstone株式会社">
+    <meta property="og:description" content="WilliamGladstone株式会社">
+    <meta property="og:image" content="">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.0.5/remodal.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.0.5/remodal-default-theme.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+  </head>
+  <body>
+    <main>
+      <section class="firstview">
+        <div class="wrapper">
+          <h1 class="serif">ウィリアム・グラッドストン</h1>
+          <p>ウィリアム・グラッドストンは世界第３位の経済大国である日本のさらなる成長促進に向けてのインパクト創出に貢献しています。</p>
+        </div>
+      </section>
+      <section class="profile thin">
+        <div class="wrapper">
+          <h2 class="serif">会社概要</h2>
+          <dl>
+            <dt>企業名</dt>
+            <dd>WilliamGladstone株式会社</dd>
+          </dl>
+          <dl>
+            <dt>代表者</dt>
+            <dd>長谷川　奬（代表取締役CEO/Founder）</dd>
+          </dl>
+          <dl>
+            <dt>資本金</dt>
+            <dd>100万円</dd>
+          </dl>
+          <dl>
+            <dt>設立</dt>
+            <dd>2021年8月6日</dd>
+          </dl>
+          <dl>
+            <dt>事業内容</dt>
+            <dd>データマネジメント&データ活用コンサルティングサービスの提供</dd>
+          </dl>
+          <dl>
+            <dt>営業時間</dt>
+            <dd>平日10時00分〜19時00分</dd>
+          </dl>
+          <dl>
+            <dt>本社</dt>
+            <dd>〒160-0023　東京都新宿区西新宿1丁目26-2新宿野村ビル32階</dd>
+          </dl>
+          <dl>
+            <dt>取引銀行</dt>
+            <dd>三菱UFJ銀行</dd>
+          </dl>
+        </div>
+      </section>
+      <section class="service">
+        <div class="wrapper">
+          <h3 class="serif">事業内容</h3>
+          <p class="thin subtitle">WilliamGladstoneは、クライアントのデータビジネスの創出やデータトランスフォーメーションの推進をご支援致します。データ戦略立案から構想策定、導入支援までワンストップでコンサルティングサービスを提供しています。</p>
+          <h4 class="serif topic">DX（デジタルトランスメーション）</h4>
+          <dl>
+            <dt>
+              <img src="./images/jeshoots-com-fzOITuS1DIQ-unsplash.jpg" alt="データマネジメント・アナリティクス・戦略策定">
+            </dt>
+            <dd>
+              <h4 class="serif">データマネジメント・アナリティクス・戦略策定</h4>
+              <p class="thin">AIやIoT、デジタルトランスフォーメーションの進展により、ダークデータと呼ばれる死蔵データが今後ますます拡大していきます。RoD(Return on Data)に着目した独自のフレームワークにもとづき、データ利活用やデータガバナンス、データマネジメント、データサイエンス部門のあるべき姿などの戦略ビジョン策定をご支援致します。</p>
+              <p class="thin">データ戦略実行のため、データ分析基盤の構築やデータテクノロジー導入支援、オペレーションの改善を通して、データトランスフォーメーションを推進致します。</p>
+              <p class="thin">ビッグデータと高度なアナリティクスによってデータドリブン経営を実現し、企業の課題解決を多面的に支援します。また、先進的なAIを活用し、ビジネスの高度化を支援致します。</p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <img src="./images/xu-haiwei-fv1EFjgIb94-unsplash.jpg" alt="量子アニーリング">
+            </dt>
+            <dd>
+              <h4 class="serif">量子アニーリング</h4>
+              <p class="thin">AIと量子コンピューターを活用し、組み合せ最適化のアルゴリズムで高精度なビッグデータ解析を実現します。金融・製造・交通・科学など多くの産業における大規模な複雑化する解析業務において、機械学習や組合せ最適の提案を行う。</p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <img src="./images/alvaro-reyes-qWwpHwip31M-unsplash.jpg" alt="システムエンジニアリングサービス（SES）">
+            </dt>
+            <dd>
+              <h4 class="serif">システムエンジニアリングサービス（SES）</h4>
+              <p class="thin">ユーザー企業やSIerのお客様の要求に直接的にお応えできるサービスとして、ITスペシャリストを提供しております。</p>
+              <p class="thin">お客様のプロジェクトチームの一員としてシステム構築に関わる、設計・開発・運用を担当する技術者を必要な期間ご提供するサービスです。お客様先に常駐するものの、派遣とは違い、エンジニアの技術力を提供しながらも、労務管理や指揮命令系統は当社にあるため、管理の負荷を軽減できます。</p>
+            </dd>
+          </dl>
+
+          <h4 class="serif topic">宇宙</h4>
+          <dl>
+            <dt>
+              <img src="./images/satellite-gc89e90355.jpg" alt="衛星データ解析">
+            </dt>
+            <dd>
+              <h4 class="serif">衛星データ解析</h4>
+              <p class="thin">地球観測衛星から得られたデータのAI解析を行なっています。衛星データの活用により様々な処理を自動化し、人間を超える認識能力で、さらに多くの情報を引き出し、全地球の問題解決に寄与します。衛星画像に機械学習や深層学習を適用して物体認識などの各種解析を行い、精密農業、エリアマーケティング、災害時の早急な被害把握など幅広くソリューション提案する。</p>
+            </dd>
+          </dl>
+          <dl>
+            <dt>
+              <img src="./images/digital-marketing-geea4bf058.jpg" alt="システムエンジニアリングサービス（SES）">
+            </dt>
+            <dd>
+              <h4 class="serif">システムエンジニアリングサービス（SES）</h4>
+              <p class="thin">宇宙関連企業やSIerのお客様の要求に直接的にお応えできるサービスとして、ITスペシャリストを提供しております。</p>
+              <p class="thin">お客様のプロジェクトチームの一員としてシステム構築・データ処理や解析に関わる、設計・開発・運用を担当する技術者を必要な期間ご提供するサービスです。お客様先に常駐するものの、派遣とは違い、エンジニアの技術力を提供しながらも、労務管理や指揮命令系統は当社にあるため、管理の負荷を軽減できます。</p>
+            </dd>
+          </dl>
+        </div>
+      </section>
+      <section class="partner">
+        <div class="wrapper">
+          <h3 class="serif">パートナー企業</h3>
+          <ul>
+            <li><img src="./images/nipponsteel3.jpeg" alt="日本製鉄株式会社"></li>
+            <li><img src="./images/kadokawa.jpeg" alt="KADOKAWA"></li>
+            <li><img src="./images/tmp-8100b64612b9038353eb4cdf946fddbd-ba40255c61e9c85c36e709bf566dc654.jpeg" alt="三井不動産"></li>
+          </ul>
+        </div>
+      </section>
+      <section class="contact">
+        <div class="wrapper">
+          <p class="serif">ウィリアム・グラッドストンへのお問い合わせ</p>
+          <a href="mailto:hasegawa@williamgladstone.co">お問い合わせ</a>
+        </div>
+      </section>
+      <footer class="footer">
+        <div class="wrapper">
+          <div class="footer_flex">
+            <div>
+              <ul class="footer_others">
+                <li>
+                  <a class="thin" href="#modal1">情報セキュリティ基本方針</a>
+                </li>
+                <li>
+                  <a class="thin" href="#modal2">プライバシーポリシー</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p class="serif name">ウィリアム・グラッドストン株式会社</p>
+              <p class="copyright">2021 WilliamGladstone Insights</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </main>
+    <div class="remodal" data-remodal-id="modal1">
+      <button data-remodal-action="close" class="remodal-close"></button>
+      <h6 class="content_modal_title serif">情報セキュリティ基本方針</h6>
+      <div class="content_modal">
+        <p class="title">情報セキュリティ管理体制の構築</p>
+        <p class="thin">当社が保有する全ての情報資産の保護に努め、情報セキュリティの維持、向上に取り組みます。</p>
+        <p class="title">情報資産の適切な管理</p>
+        <p class="thin">保有する情報資産について機密性・完全性・可用性の観点から分類・リスク評価を行い、リスクに応じて適切に管理します。</p>
+        <p class="title">法令等の遵守</p>
+        <p class="thin">情報セキュリティに関する法令およびその他のガイドラインを遵守します。</p>
+        <p class="title">継続的な情報セキュリティマネジメントの改善</p>
+        <p class="thin">当社は、定期的に監査を実施し、継続的に情報セキュリティマネジメントシステムの改善を行います。</p>
+        <p class="title">従業員へのセキュリティ教育</p>
+        <p class="thin">当社では、セキュリティに関する従業員教育を推進しています。<br>社内イントラネット上にセキュリティ情報を発信する特設サイトを設け、セキュリティに関する情報発信や啓蒙を行ったり、様々な観点からセキュリティの重要性をわかりやすく解説した漫画を公開するなど、全従業員の意識の向上に努めています。</p>
+      </div>
+      <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+    </div>
+    <div class="remodal" data-remodal-id="modal2">
+      <button data-remodal-action="close" class="remodal-close"></button>
+      <h6 class="content_modal_title serif">プライバシーポリシー</h6>
+      <div class="content_modal">
+        <p class="thin">当社は、事業活動を通じてお客様から取得する個人情報及び当社従業者の個人情報（以下、「個人情報」という。）は、当社にとって重要な情報であり、その個人情報を確実に保護することは、当社の重要な社会的責務と認識しております。<br>したがって、当社は、事業活動を通じて取得する個人情報を、以下の方針に従って取り扱い、個人情報保護に関して、お客様及び当社従業者への「安心」の提供及び社会的責務を果たしていきます。</p>
+        <p class="title">個人情報保護方針</p>
+        <p class="thin">当社は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。</p>
+        <p class="title">個人情報の管理</p>
+        <p class="thin">当社は、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</p>
+        <p class="title">個人情報の利用目的</p>
+        <p class="thin">本ウェブサイトでは、お客様からのお問い合わせ時に、お名前、e-mailアドレス、電話番号等の個人情報をご登録いただく場合がございますが、これらの個人情報はご提供いただく際の目的以外では利用いたしません。<br>お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。</p>
+        <p class="title">クッキー・IPアドレス情報</p>
+        <p class="thin">クッキー及びＩＰアドレス情報については、それら単独では特定の個人を識別することができないため、個人情報とは考えておりません。ただしこれら情報と個人情報が一体となって使用される場合にはこれら情報も個人情報とみなします。また、クッキー情報については、ブラウザの設定で拒否することが可能です。</p>
+        <p class="title">個人情報の第三者への開示・提供の禁止</p>
+        <p class="thin">当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。<br>・お客さまの同意がある場合<br>・お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合<br>・法令に基づき開示することが必要である場合</p>
+        <p class="title">個人情報の安全対策</p>
+        <p class="thin">当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。</p>
+        <p class="title">ご本人の照会</p>
+        <p class="thin">お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。</p>
+        <p class="title">法令、規範の遵守と見直し</p>
+        <p class="thin">当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</p>
+        <p class="title">お問い合せ</p>
+        <p class="thin">当社の個人情報の取扱に関するお問い合せは下記までご連絡ください。<br><a href="mailto:hasegawa@williamgladstone.co">hasegawa@williamgladstone.co</p>
+      </div>
+      <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+    </div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.0.5/remodal.min.js"></script>
+    <!-- <script src="./js/jquery-2.1.3.min.js"></script> -->
+    <!-- <script src="./js/bundle.js"></script> -->
+  </body>
+</html>
